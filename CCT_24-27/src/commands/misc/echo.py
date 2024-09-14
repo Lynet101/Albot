4ef07b3a133b513dcaab25874a70ee4e5a978247
Lynet101 <slindauskands@gmail.com>
2024-09-14 08:04:02 +0200

    Implemented "misc/echo.py"

--- CCT_24-27/src/commands/misc/echo.py
+++ CCT_24-27/src/commands/misc/echo.py
@@ -7,3 +7,16 @@
     slinda24@student.aau.dk
 """
 
+import discord
+from discord.ext import commands
+
+class Echo(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+@commands.slash_command(name="echo", description="Echoes the provided message.")
+async def echo(self, ctx: discord.ApplicationContext, message: str):
+    await ctx.send(message)
+
+def setup(bot):
+    bot.add_cog(Echo(bot))
