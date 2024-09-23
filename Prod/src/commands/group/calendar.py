64f1e484403362831c1d312c7b0e9bc495d6f424
Sebastian Lindau-Skands <47390147+Lynet101@users.noreply.github.com>
2024-09-23 07:44:33 +0200

    Added group functionallity

--- Prod/src/commands/group/calendar.py
+++
@@ -1,9 +0,0 @@
-"""
-    Command intended to create and manage group calendars for the Px projects.
-    will include creating, updating, syncing, deleting, and listing calendars.
-    Sep 14 2024 @ 07:40
-    group/calendar.py v0
-
-    Sebastian Lindau-Skands
-    slinda24@student.aau.dk
-"""
--- Prod/src/commands/group/remove.py
+++ Prod/src/commands/group/remove.py
@@ -1,7 +1,7 @@
 """
-    Adds specified members to a group.
+    Removes specified members from a group.
     Sep 17 2024 @ 15:20
-    group.add.py v1
+    group.remove.py v1
 
     Sebastian Lindau-Skands
     slinda24@student.aau.dk
@@ -10,43 +10,42 @@
 #Issues: Working with user ID, but not with tag
 import discord
 from discord.ext import commands
-
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 gid = int(os.getenv('Guild_id'))  # Ensure the guild ID is an integer
 
-class Add(commands.Cog):
+class Remove(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     #main command
-    @commands.slash_command(guild_ids=[gid], name = "group_add", description="Add members to a group")
-    async def add(self, ctx, name: str, members: str):
+    @commands.slash_command(guild_ids=[gid], name = "group_remove", description = "remove members from a group")
+    async def remove(self, ctx, name: str, members: str):
         role = discord.utils.get(ctx.guild.roles, name=f'G-{name}')
         if not role:
             await ctx.respond(f"Group G-{name} does not exist.", ephemeral=True, delete_after=3)
             return
         if not ctx.user in role.members:
-            await ctx.respond("You need to be part of this group, before you can add members.", ephemeral=True, delete_after=3)
-            print(f"Attempt at illegal group member addition by {ctx.author.name} ({ctx.author.id})")
+            await ctx.respond("You need to be part of this group, before you can remove members.", ephemeral=True, delete_after=3)
+            print(f"Attempt at illegal group member removal by {ctx.author.name} ({ctx.author.id})")
             return
-            
-        await self.add_members(ctx, role, members, name)
+        
+        await self.remove_members(ctx, role, members, name)
 
-    async def add_members(self, ctx, role, members, name):
+    async def remove_members(self, ctx, role, members, name):
         mem_num = 0
         members_ids = [id.strip() for id in members.split(",")]
 
         for member_id in members_ids:
             member = await ctx.guild.fetch_member(member_id)
             if member:
-                await member.add_roles(role)
+                await member.remove_roles(role)
                 mem_num += 1
             else:
                 await ctx.respond(f"User with ID {member_id} not found in the server.", ephemeral=True, delete_after=3)
-        await ctx.respond(f"Added {mem_num} members to the group G-{name}", ephemeral=True, delete_after=3)
+        await ctx.respond(f"Removed {mem_num} members to the group G-{name}", ephemeral=True, delete_after=3)
 
 def setup(bot):
-    bot.add_cog(Add(bot))
+    bot.add_cog(Remove(bot))
