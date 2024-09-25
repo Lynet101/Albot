0b26910403edd91a8613db0f98478e35e96cf814
Sebastian Lindau-Skands <47390147+Lynet101@users.noreply.github.com>
2024-09-25 13:48:53 +0200

    continuos dev on rust

--- Rust_frontend/aau-bot/src/main.rs
+++ Rust_frontend/aau-bot/src/main.rs
@@ -17,17 +17,14 @@ impl EventHandler for Bot {
 #[shuttle_runtime::main]
 async fn serenity(
     #[shuttle_runtime::Secrets] secrets: SecretStore,) -> shuttle_serenity::ShuttleSerenity {
-    // Get the discord token set in `Secrets.toml`
     let token = secrets
         .get("DISCORD_TOKEN")
         .context("'DISCORD_TOKEN' was not found")?;
 
-    // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+    let intents = GatewayIntents::all();
 
     let client = Client::builder(&token, intents)
-        .event_handler(Bot)
-        .await
+        .event_handler(Bot).await
         .expect("Err creating client");
 
     Ok(client.into())
