ef042be5d3c4edde2a0729844b477f4c22d3e75f
Sebastian Lindau-Skands <47390147+Lynet101@users.noreply.github.com>
2024-09-27 09:40:36 +0200

    attempted to add /purge (did not work)

--- Rust_frontend/aau-bot/src/commands/admin_main.rs
+++ Rust_frontend/aau-bot/src/commands/admin_main.rs
@@ -1,7 +1,7 @@
-use crate::commands::admin::{echo, ping};
+use crate::commands::admin::{echo, ping}; //, purge};
 use crate::types::{Error, Context};
 
-#[poise::command(prefix_command, slash_command, subcommands("echo::echo", "ping::ping"))]
+#[poise::command(prefix_command, slash_command, subcommands("echo::echo", "ping::ping"))]//, "purge::purge"))]
 pub async fn admin(_ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
